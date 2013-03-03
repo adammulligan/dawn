fce24384ad74b6a3522a287920d302b60d2cb438
Adam Mulligan <adammulligan@gmail.com>
2013-03-03 15:37:12 +0000

    Added some attributes required by PT workstation

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default['dawn']['base_dir'] = "/Applications"
+default['dawn']['cache_dir'] = Chef::Config['file_cache_path']
---
+++ attributes/versions.rb
@@ -0,0 +1,3 @@
+node.default['versions']= {
+  "homebrew" => "332d9b2e46d568f100d8b1690821aff092b3e5f6"
+}
