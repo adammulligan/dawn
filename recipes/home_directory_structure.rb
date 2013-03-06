e39c1eebac86105d0fc493d7519c682dc22eb264
Adam Mulligan <adammulligan@gmail.com>
2013-03-06 12:33:44 +0000

    Added home directory structure recipe

---
+++ recipes/home_directory_structure.rb
@@ -0,0 +1,35 @@
+if node["platform"] == "mac_os_x"
+  links = {
+    "#{WS_HOME}/Downloads" => "#{WS_HOME}/inbox"
+  }
+
+  links.each do |f, t|
+    link f do
+      to t
+    end
+  end
+end
+
+new_directories = [
+  "#{WS_HOME}/bin"
+  "#{WS_HOME}/tmp"
+]
+
+if node["platform"] == "mac_os_x"
+  new_directories.concat([
+    "#{WS_HOME}/cloud"
+  ])
+else
+  new_directories.concat([
+    "#{WS_HOME}/inbox"
+  ])
+end
+
+new_directories.each do |dir|
+  directory dir do
+    owner WS_USER
+    mode 0755
+    action :create
+    recursive true
+  end
+end
