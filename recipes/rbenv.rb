eff465ab396d62a5c00ba1cf8fd9077d54da8e68
Adam Mulligan <adammulligan@gmail.com>
2013-03-06 20:50:42 +0000

    rbenv recipe

---
+++ recipes/rbenv.rb
@@ -0,0 +1,19 @@
+include_recipe "dawn::git"
+
+::RBENV_HOME = "#{WS_HOME}/.rbenv"
+::RBENV_COMMAND = "/usr/local/bin/rbenv"
+
+brew "rbenv"
+brew "ruby-build"
+
+node["rbenv"]["rubies"].each do |version, options|
+  rbenv_ruby_install version do
+    options options
+  end
+end
+
+execute "making #{node["rbenv"]["default_ruby"]} with rbenv the default" do
+  not_if { node["rbenv"]["default_ruby"].nil? }
+  command "rbenv global #{node["rbenv"]["default_ruby"]}"
+  user WS_USER
+end
