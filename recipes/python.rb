fdced117ceeaa27b9f7610a0d595b620f9c82598
Adam Mulligan <adammulligan@gmail.com>
2013-03-08 14:53:23 +0000

    Added brew install to python recipe

--- recipes/python.rb
+++ recipes/python.rb
@@ -18,26 +18,32 @@
 # limitations under the License.
 #
 
-major_version = node['platform_version'].split('.').first.to_i
+include_recipe "dawn::homebrew"
 
-# COOK-1016 Handle RHEL/CentOS namings of python packages, by installing EPEL
-# repo & package
-if platform_family?('rhel') && major_version < 6
-  include_recipe 'yum::epel'
-  python_pkgs = ["python26", "python26-devel"]
-  node.set['python']['binary'] = "/usr/bin/python26"
+if node["platform"] != "mac_os_x"
+  brew "python"
 else
-  python_pkgs = value_for_platform_family(
-                  "debian" => ["python","python-dev"],
-                  "rhel" => ["python","python-devel"],
-                  "freebsd" => ["python"],
-                  "smartos" => ["python27"],
-                  "default" => ["python","python-dev"]
-                )
-end
+  major_version = node['platform_version'].split('.').first.to_i
 
-python_pkgs.each do |pkg|
-  package pkg do
-    action :install
+  # COOK-1016 Handle RHEL/CentOS namings of python packages, by installing EPEL
+  # repo & package
+  if platform_family?('rhel') && major_version < 6
+    include_recipe 'yum::epel'
+    python_pkgs = ["python26", "python26-devel"]
+    node.set['python']['binary'] = "/usr/bin/python26"
+  else
+    python_pkgs = value_for_platform_family(
+                    "debian" => ["python","python-dev"],
+                    "rhel" => ["python","python-devel"],
+                    "freebsd" => ["python"],
+                    "smartos" => ["python27"],
+                    "default" => ["python","python-dev"]
+                  )
+  end
+
+  python_pkgs.each do |pkg|
+    package pkg do
+      action :install
+    end
   end
 end
