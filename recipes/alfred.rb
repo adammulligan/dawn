c12b23c3a8fa166c3c2e564b01661abaedc61ae9
Adam Mulligan <adammulligan@gmail.com>
2013-03-10 09:36:24 +0000

    Fixed alred recipe

--- recipes/alfred.rb
+++ recipes/alfred.rb
@@ -1,6 +1,14 @@
-dmg_package "Alfred" do
-  volumes_dir "Alfred.app"
+remote_file "#{Chef::Config[:file_cache_path]}/alfred.zip" do
   source "http://cachefly.alfredapp.com/alfred_1.3.3_267.zip"
-  action :install
   owner WS_USER
+end
+
+execute "unzip alfred" do
+  command "unzip #{Chef::Config[:file_cache_path]}/alfred.zip -d #{Chef::Config[:file_cache_path]}/"
+  user WS_USER
+end
+
+execute "Install spotify" do
+  command "cp #{Chef::Config[:file_cache_path]}/Alfred.app /Applications"
+  user WS_USER
 end
