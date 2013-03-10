86ad5ea4cd5e5dd378152cbd58c1b028e73f40ce
Adam Mulligan <adammulligan@gmail.com>
2013-03-10 19:16:55 +0000

    Fixed hub recipe

--- recipes/hub.rb
+++ recipes/hub.rb
@@ -4,6 +4,7 @@ if node["platform"] != "mac_os_x"
   remote_file "#{WS_HOME}/bin/hub" do
     source "http://defunkt.io/hub/standalone"
     owner WS_USER
+    mode "0744"
   end
 else
   brew_install "hub"
