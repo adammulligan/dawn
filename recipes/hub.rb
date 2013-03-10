a20095a80029dba533341916028af14d850da6e8
Adam Mulligan <adammulligan@gmail.com>
2013-03-10 19:09:34 +0000

    Hub recipe

---
+++ recipes/hub.rb
@@ -0,0 +1,10 @@
+include_recipe "dawn::homebrew"
+
+if node["platform"] != "mac_os_x"
+  remote_file "#{WS_HOME}/bin/hub" do
+    source "http://defunkt.io/hub/standalone"
+    owner WS_USER
+  end
+else
+  brew_install "hub"
+end
