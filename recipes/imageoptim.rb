6982235a34eadde82f9b8f8f2b7efe1ad6f767b0
Adam Mulligan <adammulligan@gmail.com>
2013-03-10 10:08:24 +0000

    ImageOptim recipe

---
+++ recipes/imageoptim.rb
@@ -0,0 +1,14 @@
+remote_file "#{Chef::Config[:file_cache_path]}/imageoptim.tbz2" do
+  source "http://imageoptim.com/ImageOptim.tbz2"
+  owner WS_USER
+end
+
+execute "untar imageoptim" do
+  command "tar zxvf #{Chef::Config[:file_cache_path]}/imageoptim.tbz2 -d #{Chef::Config[:file_cache_path]}/"
+  user WS_USER
+end
+
+execute "Install spotify" do
+  command "cp -R #{Chef::Config[:file_cache_path]}/ImageOptim.app /Applications"
+  user WS_USER
+end
