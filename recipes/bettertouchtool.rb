c1a235cb208666784bcff767a6dc98aca18b3b0e
Adam Mulligan <adammulligan@gmail.com>
2013-03-10 10:35:03 +0000

    BetterTouchTool

---
+++ recipes/bettertouchtool.rb
@@ -0,0 +1,11 @@
+remote_file "#{Chef::Config[:file_cache_path]}/BetterTouchTool.zip" do
+  source "http://www.boastr.de/BetterTouchTool.zip"
+  owner WS_USER
+  action :create_if_missing
+end
+
+execute "unzip BetterTouchTool to /Applications" do
+  command "unzip -o #{Chef::Config[:file_cache_path]}/BetterTouchTool.zip -x __MACOSX* -d /Applications/"
+  user WS_USER
+  group "admin"
+end
